1b19d524576bb6b486c8d244111dbfc3db9aa6f0
Johnny Kingi <jknz84@gmail.com>
2018-11-12 16:12:30 +1300

    CRUD actions and pages created

--- awoloo/app/controllers/products_controller.rb
+++ awoloo/app/controllers/products_controller.rb
@@ -1,22 +1,50 @@
 class ProductsController < ApplicationController
+    #display all products in database
     def index
         @products = Product.all
     end
+    #show selected product in database
     def show
         @product = Product.find(params[:id])
     end
-
+    #to add new product in database
     def new
+        @product = Product.new
     end
-
+    #to edit product in database
+    def edit
+        @product = Product.find(params[:id])
+    end
+    #to create new product in database with required product_params
     def create
         @product = Product.new(product_params)
 
-        @product.save
-        redirect_to @product
+        #if statement to check validation of model (min title >= 5)
+        if @product.save
+            redirect_to @product
+        else
+            render 'new'
+        end
+
         #render plain: params[:product].inspect -use this command to view parameter data
     end
+    #method to update selected product by id
+    def update
+        @product = Product.find(params[:id])
 
+        if @product.update(product_params)
+            redirect_to @product
+        else
+            render 'edit'
+        end
+    end
+    #method to delete resources
+    def destroy
+        @product = Product.find(params[:id])
+        @product.destroy
+
+        redirect_to products_path
+    end
     private
   def product_params
     params.require(:product).permit(:title, :subtitle, :description)
--- awoloo/app/models/product.rb
+++ awoloo/app/models/product.rb
@@ -1,2 +1,5 @@
 class Product < ApplicationRecord
+    validates :title, presence: true, length: {minimum: 5}
+    validates :subtitle, presence: true 
+    validates :description, presence: true
 end
